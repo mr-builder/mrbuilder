1a4fb24e4dde33a9d4f06c5bed8e1bbf77349282
Justin Spears <jspears@netflix.com>
2018-02-13 18:41:25 -0800

    go back to old style env

--- core/mrbuilder/bin/mrbuilder-webpack-dev-server.js
+++ core/mrbuilder/bin/mrbuilder-webpack-dev-server.js
@@ -1,3 +1,5 @@
-#!/usr/bin/env -S MRBUILDER_INTERNAL_PRESETS=${MRBUILDER_INTERNAL_PRESETS},mrbuilder-preset-app NODE_ENV=development node
+#!/usr/bin/env node
+process.env.MRBUILDER_INTERNAL_PRESETS=`${process.env.MRBUILDER_INTERNAL_PRESETS || ''},mrbuilder-preset-app`;
+process.env.MRBUILDER_ENV = process.env.MRBUILDER_ENV || 'development';
 global._MRBUILDER_OPTIONS_MANAGER = global._MRBUILDER_OPTIONS_MANAGER  || new (require('mrbuilder-optionsmanager').default)({ prefix: 'mrbuilder', _require: require });
 require('mrbuilder-plugin-webpack-dev-server/bin/cli');
