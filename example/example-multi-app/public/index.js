e01d064bbc18dc90942ca2ffc5d68049133a3df1
Justin Spears <jspears@netflix.com>
2018-02-13 18:39:47 -0800

    make example more thorough

--- example/example-multi-app/public/index.js
+++ example/example-multi-app/public/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import shared from './shared';
+import Stylus from './app.stylm';
+import Less from './example.lessm'
+
 
 const init = () => render(<div >
     <h1>Hello from page 1</h1>
-    <a href="other.html">Other Page</a>
-    <p>This uses rewrite to turn a frown upside down</p>
+    <a className={Stylus.link} href="other.html">Other Page</a>
+    <p className={Less.widget}>This uses rewrite to turn a frown upside down</p>
     <img src={"/frown.png"}/>
     <h2>{shared()}</h2>
 </div>, document.getElementById('content'));
