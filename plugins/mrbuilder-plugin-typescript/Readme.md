927936818d3bd8f65bdd0ab098c690144e9a3b1f
Justin Spears <jspears@netflix.com>
2018-03-05 16:13:09 -0800

    added typescript

---
+++ plugins/mrbuilder-plugin-typescript/Readme.md
@@ -0,0 +1,4 @@
+Provides typescript
+
+** If this tsconfig does not work, please add one to your project dir. still
+figuring this out. **
---
+++ plugins/mrbuilder-plugin-typescript/src/index.js
@@ -0,0 +1,40 @@
+const path                = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const { existsSync }      = require('fs');
+
+module.exports = function ({
+                               test = /\.tsx?$/,
+                               baseUrl = "./",
+                               extensions = [".ts", ".tsx"],
+                               configFile = path.join(__dirname,
+                                   '..',
+                                   'tsconfig.json')
+                           },
+                           webpack, om) {
+    webpack.module.rules.push(
+        {
+            test,
+            use:[
+                'source-map-loader',
+                'ts-loader',
+            ]
+        }
+    );
+
+    if (extensions) {
+        if (!webpack.resolve.extensions) {
+            webpack.resolve.extensions = [];
+        }
+        webpack.resolve.extensions.push(...extensions)
+    }
+    if (!existsSync(om.cwd('tsconfig.json'))) {
+        if (configFile) {
+            this.info(`using tsconfig ${configFile} with baseUrl ${baseUrl}`)
+            webpack.plugins.unshift(
+                new TsconfigPathsPlugin({ configFile, baseUrl,extensions,  }));
+        }
+    }else{
+        this.info('using local tsconfig.json');
+    }
+    return webpack;
+};
