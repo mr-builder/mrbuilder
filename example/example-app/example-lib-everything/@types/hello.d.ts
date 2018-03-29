77768fb0f46dce7ca249ac1904ac01efa155631b
Justin Spears <jspears@netflix.com>
2018-03-29 10:42:49 -0700

    make typescript happy

---
+++ example/example-app/example-lib-everything/@types/hello.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="react" />
+import * as React from 'react';
+export interface HelloProps {
+    name: string;
+}
+export declare type HelloState = {
+    value?: number;
+};
+export declare class Hello extends React.Component<HelloProps, HelloState> {
+    state: {
+        value: number;
+    };
+    handleIncrement: () => void;
+    handleDecrement: () => void;
+    render(): JSX.Element;
+}
