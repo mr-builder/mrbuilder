5fbf74d6601a6a2746ce4b788e6cfc0f2b54f55b
Justin Spears <jspears@netflix.com>
2018-04-27 16:19:08 -0700

    v3 beta

---
+++ plugins/mrbuilder-plugin-babel/bin/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+if (!global._MRBUILDER_OPTIONS_MANAGER) {
+    process.env.MRBUILDER_INTERNAL_PLUGINS=`${process.env.MRBUILDER_INTERNAL_PLUGINS ||''},mrbuilder-plugin-babel`;
+    global._MRBUILDER_OPTIONS_MANAGER =
+        new (require('mrbuilder-optionsmanager').default)
+}
+const { plugins, presets }        = require(
+    'mrbuilder-plugin-babel/babel-config');
+const { argv }                    = process;
+if (!argv.includes('-s')) {
+    argv.push('-s', 'true');
+}
+if (!(argv.includes('--out-file') || argv.includes('--out-dir'))) {
+    argv.push('--out-dir', 'lib');
+}
+if (!(argv.includes('--copy-files') || argv.includes('-D'))) {
+    argv.push('--copy-files');
+}
+if (!argv.includes('--presets')) {
+    argv.push('--presets', presets);
+}
+if (!argv.includes('--plugins')) {
+    argv.push('--plugins', plugins);
+}
+if (!(argv.includes('--out-file') || argv.includes('--filename'))) {
+    argv.push('src');
+}
+require('babel-cli/bin/babel');
