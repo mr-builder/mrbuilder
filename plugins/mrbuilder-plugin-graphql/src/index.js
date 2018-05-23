0cd514d72115b8f142db50aa568ddb94eee73f5b
Justin Spears <jspears@netflix.com>
2018-05-23 09:26:57 -0700

    err wrong wepack version

--- plugins/mrbuilder-plugin-graphql/src/index.js
+++ plugins/mrbuilder-plugin-graphql/src/index.js
@@ -2,14 +2,16 @@ module.exports = function graphql({
                                       test = /\.(graphql|gql)$/,
                                       loader = 'graphql-tag/loader',
                                       exclude = /node_modules/,
-                                      ...rest
-                                  }={}, webpack) {
+                                      ...options
+                                  } = {}, webpack) {
 
-    webpack.plugins.loaders.push({
+    webpack.module.rules.push({
         test,
         exclude,
-        loader,
-        ...rest,
+        use: {
+            loader,
+            options,
+        },
     });
 
     return webpack;
