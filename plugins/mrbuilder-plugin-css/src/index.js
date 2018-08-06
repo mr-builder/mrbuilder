7d16e4cb08f3f4edbcec1b8e500f1120942eb4e1
Justin <speajus@gmail.com>
2018-08-06 11:51:26 -0700

    Styleguide cleanup (#36)
    * fix lerna 2
    
    * v3.1.1
    
    * v3.1.2
    
    * fixes for mocha and css
    
    * added descriptions and improved styleguide component to optionally use lerna
    
    * update more descriptions
    
    * why does this break
    
    * removed unused code added example generation
    
    * way to subtle fix for next release

--- plugins/mrbuilder-plugin-css/src/index.js
+++ plugins/mrbuilder-plugin-css/src/index.js
@@ -4,7 +4,7 @@ module.exports  = function ({
                                 useNameHash,
                                 publicPath = '/public',
                                 css = true,
-                                modules = false,
+                                modules,
                             }, webpack, om) {
 
     if (css) {
--- plugins/mrbuilder-plugin-css/test/css-test.js
+++ plugins/mrbuilder-plugin-css/test/css-test.js
@@ -37,7 +37,7 @@ describe('mrbuilder-plugin-css', function () {
         const ctx = {};
         const mod = require('../src');
 
-        mod.call(ctx, {modules: true}, webpack, FAKE);
+        mod.call(ctx, {modules: true, }, webpack, FAKE);
 
         expect(webpack.module.rules).to.have.length(2);
 
