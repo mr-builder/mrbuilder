8de5f8664acac91230394d317e062583ed88a02a
Justin Spears <jspears@netflix.com>
2018-12-15 15:45:43 -0800

    fixed mocha with babel

--- plugins/mrbuilder-plugin-mocha/bin/cli.js
+++ plugins/mrbuilder-plugin-mocha/bin/cli.js
@@ -3,31 +3,24 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 if (!global._MRBUILDER_OPTIONS_MANAGER) {
     process.env.MRBUILDER_INTERNAL_PLUGINS =
         `${process.env.MRBUILDER_INTERNAL_PLUGINS || ''},mrbuilder-plugin-enzyme,mrbuilder-plugin-mocha`;
-    global._MRBUILDER_OPTIONS_MANAGER      =
-        new (require('mrbuilder-optionsmanager').default)({
-            prefix  : 'mrbuilder',
-            _require: require
-        });
-
+    global._MRBUILDER_OPTIONS_MANAGER      = new (require('mrbuilder-optionsmanager').default)({
+        prefix  : 'mrbuilder',
+        _require: require
+    });
 }
-const optionsManager = global._MRBUILDER_OPTIONS_MANAGER;
 
-
+const om     = global._MRBUILDER_OPTIONS_MANAGER;
 const {cwd}  = require('mrbuilder-utils');
 const path   = require('path');
 const {argv} = process;
-const {info} = optionsManager.plugins.get('mrbuilder-plugin-mocha');
+const {info} = om.plugins.get('mrbuilder-plugin-mocha');
 
-const coverageDir    = optionsManager.config(
-    'mrbuilder-plugin-mocha.coverageDir');
-const coverageGLobal = optionsManager.config(
-    'mrbuilder-plugin-mocha.coverageGLobal');
-const testDir        = optionsManager.config('mrbuilder-plugin-mocha.testDir', cwd('test'));
-const filePattern    = optionsManager.config(
-    'mrbuilder-plugin-mocha.filePattern', '**/*-test.js');
-const timeout        = optionsManager.config('mrbuilder-plugin-mocha.timeout',
-    20000);
-const useBabel       = optionsManager.config('mrbuilder-plugin-mocha.useBabel', optionsManager.enabled('mrbuilder-plugin-babel'));
+const coverageDir    = om.config('mrbuilder-plugin-mocha.coverageDir');
+const coverageGLobal = om.config('mrbuilder-plugin-mocha.coverageGLobal');
+const testDir        = om.config('mrbuilder-plugin-mocha.testDir', cwd('test'));
+const filePattern    = om.config('mrbuilder-plugin-mocha.filePattern', '**/*-test.js');
+const timeout        = om.config('mrbuilder-plugin-mocha.timeout', 20000);
+const useBabel       = om.config('mrbuilder-plugin-mocha.useBabel', om.enabled('mrbuilder-plugin-babel'));
 info(`running tests '${testDir}/${filePattern}'`);
 
 let mocha;
@@ -47,11 +40,7 @@ if (coverageDir || coverageGLobal) {
     mocha = path.resolve(__dirname, '..', 'node_modules', '.bin', 'nyc');
 } else {
     if (useBabel) {
-        if (require('mrbuilder-plugin-babel/version') > 6 ) {
-            argv.push('--require', require.resolve('@babel/polyfill'));
-        }else{
-            argv.push('--require', require.resolve('babel-polyfill'));
-        }
+        argv.push('--require', require.resolve('mrbuilder-plugin-babel/babel-polyfill'));
     }
 }
 
@@ -63,13 +52,13 @@ if (useBabel) {
     argv.push('--require', require.resolve('mrbuilder-plugin-babel/babel-register'));
 }
 
-if (optionsManager.enabled('mrbuilder-plugin-enzyme')) {
+if (om.enabled('mrbuilder-plugin-enzyme')) {
     argv.push('--require', path.join(__dirname, '..', 'src', 'cli-helpers'));
     argv.push('--require', 'mrbuilder-plugin-enzyme/src/enzyme-mocha');
 }
 
 argv.push(testDir + '/' + filePattern);
 
-optionsManager.plugins.get('mrbuilder-plugin-mocha').debug(`[mrbuilder-mocha] running with args `, argv);
+om.plugins.get('mrbuilder-plugin-mocha').debug(`[mrbuilder-mocha] running with args `, argv);
 
 require(mocha);
