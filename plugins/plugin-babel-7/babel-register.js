0528b6c3cf1e9513886e55aab4d1045835c91bd1
Justin <speajus@gmail.com>
2020-03-31 14:08:46 -0700

    Tweaks (#65)
    * woops mispelt
    
    * add babel to test profile
    
    * add defaults
    
    * add export config
    
    * better debugging
    
    * use babel by default
    
    Co-authored-by: Justin <juspears@paypal.com>

--- plugins/plugin-babel-7/babel-register.js
+++ plugins/plugin-babel-7/babel-register.js
@@ -1,3 +1,3 @@
 const config = require('./babel-config');
-require('@mrbuilder/cli').default.debug('babel', JSON.stringify(config));
+require('@mrbuilder/cli').default.logger('@mrbuilder/plugin-babel').debug(JSON.stringify(config, null, 2));
 require('@babel/register')(config);
