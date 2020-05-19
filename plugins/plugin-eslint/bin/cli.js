779eb497aff22588731308e1e324c31c48fbdb43
Justin Spears <speajus@gmail.com>
2020-05-18 17:37:28 -0700

    update cli

--- plugins/plugin-eslint/bin/cli.js
+++ plugins/plugin-eslint/bin/cli.js
@@ -4,16 +4,28 @@ const args = process.argv.slice(2);
 if (!(args.includes('-c', 2) || args.includes('--config') || args.find((v) => v.startsWith('--config=')))) {
     const configFile = optionsManager.config('@mrbuilder/plugin-eslint.configFile');
     if (configFile) {
-        process.argv.push('--config', enhancedResolve(configFile, optionsManager.require));
-    }else{
-        process.argv.push('--config', require.resolve('@mrbuilder/plugin-eslint/src/eslint'));
+        process.argv.splice(2, 0, '--config', enhancedResolve(configFile, optionsManager.require));
+    } else {
+        process.argv.splice(2, 0, '--config', require.resolve('@mrbuilder/plugin-eslint/src/eslint'));
     }
 }
 
 if (!(args.find(v => /^--fix[=-]?$/.test(v)))) {
     if (optionsManager.config('@mrbuilder/plugin-eslint.fix')) {
-        process.argv.push('--fix');
+        process.argv.splice(2, 0, '--fix');
     }
 }
+
+if (!(args.find(v => /^--ext([=].*)$/.test(v)))) {
+    const ext = optionsManager.config(
+        '@mrbuilder/plugin-eslint.ext',
+        optionsManager.config('@mrbuilder/plugin-webpack.extensions')
+    )
+    if (ext) {
+        process.argv.splice(2, 0, '--ext='+ext);
+    }
+}
+
+process.argv.push(optionsManager.config('@mrbuilder/plugin-eslint.sourceDir', optionsManager.config('@mrbuilder/cli.sourceDir', 'src')));
 
 require('eslint/bin/eslint');
