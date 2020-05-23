de05754e954d6d4e45ebccb0d868f8b0a5878c90
Justin Spears <speajus@gmail.com>
2020-05-23 16:50:45 -0700

    fix(create/plugin): work without a namespace

--- create/plugin/plopfile.js
+++ create/plugin/plopfile.js
@@ -16,13 +16,14 @@ module.exports = function (plop) {
     plop.setHelper('cwd', process.cwd);
     plop.setActionType('install', function (answers) {
         return `success\nplease run
- $ cd plugins/plugin-${answers.name}  
+ $ cd plugins/plugin-${answers.pluginName}  
  $ ${answers.useYarn ? 'yarn install' : 'npm install'}`;
     });
     plop.setHelper('extension', (v, ans) => ans.typescript ? 'ts' : 'js');
     plop.setHelper('no_at_namespace', ({data: {root: {namespace}}}) => namespace ? namespace.replace(/^@/, '') : '')
     plop.setHelper('name', ({data: {root: {pluginName}}}) => pluginName.replace(/.*plugin-/, ''));
-    plop.setHelper('pkg-name', ({data: {root: {namespace, name}}}) => namespace ? `${namespace}/plugin-${name}` : `plugin-${name}`)
+    plop.setHelper('pkg-name', ({data: {root: {namespace, pluginName}}}) => namespace ? `${namespace}/plugin-${pluginName}` : `plugin-${pluginName}`);
+    plop.setHelper('script-bin',  ({data: {root: {namespace, pluginName}}}) => namespace ? `${namespace.replace(/^@/,'')}-${pluginName}` : pluginName);
     plop.setGenerator('monorepo', {
             description: 'This is sets up a mrbuilder plugin',
             prompts: [
