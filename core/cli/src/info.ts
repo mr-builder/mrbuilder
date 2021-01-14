0c11f4259c009684ee198d0e5e2322b8543269d3
Justin Spears <speajus@gmail.com>
2021-01-14 11:26:36 -0800

    fixed up some setup

--- core/cli/src/info.ts
+++ core/cli/src/info.ts
@@ -6,24 +6,22 @@ const {
 
 const NODE_ENV = env.NODE_ENV = env.NODE_ENV || om.config('@mrbuilder/cli.node_env');
 
-export const isDevServer = om.enabled('@mrbuilder/plugin-webpack-dev-server') || om.config('@mrbuilder/cli.devServer');
+export const isDevServer = om.config('@mrbuilder/cli.devServer');
+export const isKarma = om.enabled('@mrbuilder/plugin-karma') ;
 
-
-export const isKarma = om.enabled('@mrbuilder/plugin-karma');
-
-export const isTest = NODE_ENV === 'test' || isKarma || om.enabled('@mrbuilder/plugin-mocha') || om.enabled('@mrbuilder/plugin-jest');
+export const isTest = NODE_ENV === 'test' || isKarma || om.config('@mrbuilder/cli.test');
 
 export const mode = om.config('@mrbuilder/plugin-webpack.mode',
     NODE_ENV ||
     (isTest || isDevServer) ? 'development' : 'production'
 );
 
-export const isApp = !!om.config('@mrbuilder/plugin-webpack.app'),
+export const isApp = !!om.config('@mrbuilder/plugin-webpack.app') || om.enabled('@mrbuilder/plugin-storybook.app'),
     isDemo = !!om.config('@mrbuilder/plugin-webpack.demo'),
     isHtml = om.enabled('@mrbuilder/plugin-html'),
     isHot = om.enabled('@mrbuilder/plugin-hot'),
     isTypescript = om.enabled('@mrbuilder/plugin-typescript'),
-    isDebug = om.env('DEBUG', false),
+    isDebug = env.DEBUG || om.env('DEBUG', false),
     isProduction = mode == null ? NODE_ENV === 'production' : mode
         === 'production',
 
@@ -36,7 +34,9 @@ export const isApp = !!om.config('@mrbuilder/plugin-webpack.app'),
      */
 
     isLibrary = !(isTest || isDevServer || isApp || isDemo);
-export default ({
+
+
+const setup = ({
     isTest,
     isKarma,
     isDebug,
@@ -49,3 +49,8 @@ export default ({
     isLibrary
 });
 
+if (isDebug){
+    console.log(Object.entries(setup).map(([key,value])=>`${key}\t: ${value}`).join('\n'));
+}
+
+export default setup;
